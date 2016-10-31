41ab2263e12419bc9434e3f59d1c050536c17940
Venca24 <Vaclav.Zouzalik@seznam.cz>
2016-10-31 23:01:31 +0100

    RentalDaoTest reimplemented

--- src/test/java/tests/RentalDaoTest.java
+++ src/test/java/tests/RentalDaoTest.java
@@ -12,7 +12,10 @@ import dao.RentalDaoImpl;
 import dao.UserDao;
 import entity.Machine;
 import entity.Rental;
+import entity.User;
 import enums.MachineType;
+import enums.PersonType;
+import enums.Role;
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
@@ -23,7 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  *
@@ -41,36 +44,95 @@ public class RentalDaoTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private UserDao userDao;
     
+    private Machine machine1;
+    private User usr;
+    
+    @BeforeClass
+    public void beforeClass(){
+        machine1 = new Machine();
+        machine1.setName("Cat");
+        machine1.setPricePerDay(BigDecimal.TEN);
+        machine1.setMachineType(MachineType.EXCAVATOR);
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(Calendar.YEAR, 2014);
+        cal1.set(Calendar.MONTH, Calendar.JANUARY);
+        cal1.set(Calendar.DAY_OF_MONTH, 6);
+        machine1.setDateOfBuy(cal1.getTime());
+        machine1.setDateOfLastRevision(cal1.getTime());
+        machineDao.create(machine1);
+
+        usr = new User();
+        usr.setGivenName("Albus");
+        usr.setSurname("Dumbledore");
+        usr.setPasswordHash("adfbgnh");
+        usr.setEmail("albus@hogwarts.edu");
+        usr.setPersonType(PersonType.NATURAL);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(Calendar.YEAR, 2016);
+        cal2.set(Calendar.MONTH, Calendar.JANUARY);
+        cal2.set(Calendar.DAY_OF_MONTH, 20);
+        usr.setJoinedDate(cal1.getTime());
+        usr.setPhone("800123456");
+        usr.setRole(Role.EMPLOYEE);
+        userDao.create(usr);
+    }
+    
     @Test
     public void testCreate(){
-        Rental rental = new Rental();
+        Rental rent = getRental();
+        rent.setMachine(machine1);
+        rent.setUser(usr);
+
+        rentalDao.create(rent);
+
+        Assert.assertNotNull(rent.getId());
+        Assert.assertEquals(usr, rent.getUser());
+        Assert.assertEquals(machine1, rent.getMachine());
+    }
+    
+    @Test
+    public void testfindById(){
+        Rental rent = getRental();
+        rent.setMachine(machine1);
+        rent.setUser(usr);
+
+        rentalDao.create(rent);
+        Assert.assertEquals(rent.getUser().getEmail(), rentalDao.findById(rent.getId()).getUser().getEmail());
+        Assert.assertEquals(rent.getMachine().getName(), rentalDao.findById(rent.getId()).getMachine().getName());
+        Assert.assertEquals(new Integer(5000), rentalDao.findById(rent.getId()).getPrice());
+    }
+    
+    @Test
+    public void testUpdate(){
+        Rental rent = getRental();
+        rent.setMachine(machine1);
+        rent.setUser(usr);
+        rentalDao.create(rent);
+
+        rent.setPrice(8500);
+        
+        rentalDao.update(rent);
+        Assert.assertEquals(new Integer(8500), rentalDao.findById(rent.getId()).getPrice());
+    }
+    
+    private Rental getRental() {
+        Rental rent = new Rental();
         
         Calendar cal1 = new GregorianCalendar();
-        cal1.set(2016, Calendar.OCTOBER, 30, 0, 0);
+        cal1.set(Calendar.YEAR, 2016);
+        cal1.set(Calendar.MONTH, Calendar.OCTOBER);
+        cal1.set(Calendar.DAY_OF_MONTH, 30);
+        rent.setDateFrom(cal1.getTime());
         
-        Calendar cal2 = new GregorianCalendar();
-        cal2.set(2016, Calendar.NOVEMBER, 1, 0, 0);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(Calendar.YEAR, 2016);
+        cal2.set(Calendar.MONTH, Calendar.NOVEMBER);
+        cal2.set(Calendar.DAY_OF_MONTH, 1);
+        rent.setDateTo(cal2.getTime());
+        
+        rent.setPrice(5000);
 
-        rental.setDateFrom(cal1.getTime());
-        rental.setDateTo(cal2.getTime());
-
-        rental.setPrice(2000);
-        rental.setUser(userDao.findUserByEmail("test.test@gmail.com"));
-
-        Machine machine = new Machine();
-        machine.setName("Cat");
-        machine.setDateOfBuy(cal1.getTime());
-        machine.setDateOfLastRevision(cal1.getTime());
-        machine.setPricePerDay(new BigDecimal(10));
-        machine.setMachineType(MachineType.EXCAVATOR);
-
-        rental.setMachine(machine);
-
-        machineDao.create(machine);
-
-        rentalDao.create(rental);
-        Assert.assertNotNull(rental.getId());
-        rentalDao.delete(rental);
+        return rent;
     }
 
 }
