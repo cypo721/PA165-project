8ca96f911b02ca0ee0d78d81d78ceb0e0b5fadd1
Venca24 <Vaclav.Zouzalik@seznam.cz>
2016-11-25 16:07:51 +0100

    Added comments in UserFacade and UserService

--- api-layer/src/main/java/facade/UserFacade.java
+++ api-layer/src/main/java/facade/UserFacade.java
@@ -13,15 +13,47 @@ import java.util.Collection;
  * @author Václav Zouzalík
  */
 public interface UserFacade {
+    
+    /**
+     * Create new user in the system
+     * @param userDTO
+     * @param password
+     * @return newly created user
+     */
     UserDTO createUser(UserDTO userDTO, String password);
     
+    /**
+     * Authenticate user in the system
+     * @param u
+     * @param password
+     * @return true if the authentication was successfull
+     */
     public boolean authenticate(UserDTO u, String password);
     
+    /**
+     * Check wheter a user has employee status or not
+     * @param u
+     * @return true when the user has employee status
+     */
     public boolean isEmployee(UserDTO u);
    
+    /**
+     * Find user by id
+     * @param id
+     * @return found user or null
+     */
     public UserDTO findById(Long id);
     
+    /**
+     * Find user by email
+     * @param email
+     * @return found user or null
+     */
     public UserDTO findByEmail(String email);
     
+    /**
+     * Get all users
+     * @return 
+     */
     public Collection<UserDTO> getAllUsers();
 }
--- service-layer/src/main/java/service/UserService.java
+++ service-layer/src/main/java/service/UserService.java
@@ -16,17 +16,54 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public interface UserService {
+    
+    /**
+     * Cretae a user in the system
+     * @param usr 
+     * @param password
+     * @return newly created user entity
+     */
     User createUser(User usr, String password);
     
+    /**
+     * Change password of the given user
+     * @param id of the user
+     * @param newPassword 
+     */
     void changePassword(Long id, String newPassword);
     
+    /**
+     * Authenticate user in the system
+     * @param id
+     * @param password
+     * @return true if the authentication was successfull
+     */
     boolean authenticate(Long id, String password);
     
+    /**
+     * Check wheter a user has employee status or not
+     * @param id
+     * @return true when the user has employee status
+     */
     boolean isEmployee(Long id);
     
+    /**
+     * Find user by id
+     * @param id
+     * @return found user or null
+     */
     User getUserById(Long id);
     
+    /**
+     * Find user by email
+     * @param email
+     * @return found user or null
+     */
     User getUserByEmail(String email);
     
+    /**
+     * Get all users
+     * @return 
+     */
     public List<User> getAllUsers();
 }
