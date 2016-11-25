c5da5c99de0ff403324db63f4a0c3b087bf284eb
kralmajster <marekbohm13@gmail.com>
2016-11-25 23:21:53 +0100

    Removed public modifier from interface RevisionService.java

--- service-layer/src/main/java/service/RevisionService.java
+++ service-layer/src/main/java/service/RevisionService.java
@@ -13,31 +13,31 @@ public interface RevisionService {
      * @param revision
      * @return newly created revision entity
      */
-    public Revision create(Revision revision);
+    Revision create(Revision revision);
 
     /**
      * Update a revision in the system
      * @param revision
      * @return modified revision entity
      */
-    public Revision update(Revision revision);
+    Revision update(Revision revision);
 
     /**
      * Deletes revision given in parameter
      * @param revision
      */
-    public void delete(Revision revision);
+    void delete(Revision revision);
 
     /**
      * Deletes revision given in parameter
      * @param id
      * @return revision matching id from parameter
      */
-    public Revision findById(Long id);
+    Revision findById(Long id);
 
     /**
      * Deletes revision given in parameter
      * @return list of all revisions in the system
      */
-    public List<Revision> findAllRevisions();
+    List<Revision> findAllRevisions();
 }
