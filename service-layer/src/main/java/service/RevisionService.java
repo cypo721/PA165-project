95667c2bf55fc1c97d3a66fd5950603add274602
kralmajster <marekbohm13@gmail.com>
2016-11-25 23:18:52 +0100

    Added doc to RevisionService.java

--- service-layer/src/main/java/service/RevisionService.java
+++ service-layer/src/main/java/service/RevisionService.java
@@ -8,9 +8,36 @@ import java.util.List;
  */
 public interface RevisionService {
 
+    /**
+     * Cretae a revision in the system
+     * @param revision
+     * @return newly created revision entity
+     */
     public Revision create(Revision revision);
+
+    /**
+     * Update a revision in the system
+     * @param revision
+     * @return modified revision entity
+     */
     public Revision update(Revision revision);
+
+    /**
+     * Deletes revision given in parameter
+     * @param revision
+     */
     public void delete(Revision revision);
+
+    /**
+     * Deletes revision given in parameter
+     * @param id
+     * @return revision matching id from parameter
+     */
     public Revision findById(Long id);
+
+    /**
+     * Deletes revision given in parameter
+     * @return list of all revisions in the system
+     */
     public List<Revision> findAllRevisions();
 }
