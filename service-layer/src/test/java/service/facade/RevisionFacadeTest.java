df6417991b8b07aa23be4895797f7e23b00e8fe3
Venca24 <Vaclav.Zouzalik@seznam.cz>
2016-11-25 21:31:25 +0100

    reimplementation of RevisionFacadeTest

--- service-layer/src/test/java/service/facade/RevisionFacadeTest.java
+++ service-layer/src/test/java/service/facade/RevisionFacadeTest.java
@@ -3,10 +3,10 @@ package service.facade;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 import dto.*;
+import entity.*;
 import enums.*;
 import facade.*;
 import org.junit.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.annotations.Test;
@@ -14,90 +14,101 @@ import service.config.ServiceConfiguration;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
-import java.util.Date;
+import javax.inject.Inject;
+import static org.mockito.Matchers.any;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.testng.annotations.BeforeMethod;
+import service.BeanMappingService;
+import service.*;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Václav Zouzalík
  */
 @RunWith(MockitoJUnitRunner.class)
 @ContextConfiguration(classes=ServiceConfiguration.class)
-public class RevisionFacadeTest extends AbstractTransactionalTestNGSpringContextTests {
-
-    @Autowired
-    private RevisionFacade revisionFacade;
-
-    @Autowired
-    private MachineFacade machineFacade;
-
-    @Autowired
-    private UserFacade userFacade;
-
-    private RevisionDTO testRevision;
-    private MachineDTO testMachine;
-    private UserDTO testUser;
+public class RevisionFacadeTest extends AbstractTransactionalTestNGSpringContextTests{
+        
+    @Mock
+    private RevisionService revisionService;
+    
+    @Mock
+    private MachineService machineService;
+    
+    @Mock
+    private UserService userService;
+    
+    @Spy
+    @Inject
+    private BeanMappingService mappingService;
+    
+    @InjectMocks
+    private final RevisionFacade revisionFacade = new RevisionFacadeImpl();
+    
+    @InjectMocks
+    private final MachineFacade machineFacade = new MachineFacadeImpl();
+    
+    @InjectMocks
+    private final UserFacade userFacade = new UserFacadeImpl();
+    
+    private Revision revision;
+    private Machine machine;
+    private User user;
     
     @BeforeMethod
-    public void prepareTestMachine()
-    {
-        testMachine = new MachineDTO();
-        testMachine.setName("Caterpilar");
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
         
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 1996);
-        cal.set(Calendar.MONTH, Calendar.NOVEMBER);
-        cal.set(Calendar.DAY_OF_MONTH, 24);
+        cal.set(Calendar.YEAR, 2014);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 6);
         
-        testMachine.setDateOfBuy(cal.getTime());
-        testMachine.setPricePerDay(new BigDecimal(5000));
-        testMachine.setMachineType(MachineType.EXCAVATOR);
+        machine = new Machine();
+        machine.setId(1L);
+        machine.setName("Caterpilar");
+        machine.setPricePerDay(new BigDecimal(5000));
+        machine.setMachineType(MachineType.EXCAVATOR);
+        machine.setDateOfBuy(cal.getTime());
+        machine.setDateOfLastRevision(cal.getTime());
+        
+        user = new User();
+        user.setId(1L);
+        user.setGivenName("Julia");
+        user.setSurname("Green");
+        user.setEmail("julia@green.name");
+        user.setPasswordHash("qefwre");
+        user.setPhone("56109");
+        user.setPersonType(PersonType.NATURAL);
+        user.setRole(Role.EMPLOYEE);
+        user.setJoinedDate(cal.getTime());
+        
+        revision = new Revision();
+        revision.setId(1L);
+        revision.setInfo("Test");
+        revision.setIsFunctionable(true);
+        revision.setDateOfRevision(cal.getTime());
+        revision.setMachine(machine);
+        revision.setUser(user);
     }
     
-    @BeforeMethod
-    public void prepareTestUser()
-    {
-        testUser = new UserDTO();
-        testUser.setGivenName("Julia");
-        testUser.setSurname("Green");
-        testUser.setEmail("julia@green.name");
-        testUser.setPersonType(PersonType.NATURAL);
-        testUser.setRole(Role.EMPLOYEE);
+    /*@Test
+    public void testCreateRevision() {        
+        when(machineService.create(any(Machine.class))).thenReturn(machine);
+        MachineDTO mDTO = mappingService.mapTo(machine, MachineDTO.class);
+        machineFacade.createMachine(mDTO);
         
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2016);
-        cal.set(Calendar.MONTH, Calendar.NOVEMBER);
-        cal.set(Calendar.DAY_OF_MONTH, 24);
+        when(userService.createUser(any(User.class), any(String.class))).thenReturn(user);
+        UserDTO uDTO = mappingService.mapTo(user, UserDTO.class);
+        userFacade.createUser(uDTO, "dwefwtr");
         
-        testUser.setJoinedDate(cal.getTime());
-    }
-    
-    @BeforeMethod
-    public void prepareTestRevision()
-    {
-        testRevision = new RevisionDTO();
-        
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 2016);
-        cal.set(Calendar.MONTH, Calendar.NOVEMBER);
-        cal.set(Calendar.DAY_OF_MONTH, 24);
-        
-        testRevision.setDateOfRevision(cal.getTime());
-        /*testRevision.setUser(testUser);
-        testRevision.setMachine(testMachine);*/
-    }
-    
-    @Test
-    public void testCreateAndUpdate()
-    {
-        userFacade.createUser(testUser, "dewfrt");
-        machineFacade.createMachine(testMachine);
-        testRevision.setUser(testUser);
-        testRevision.setMachine(testMachine);
-        revisionFacade.createRevision(testRevision);
-        Assert.assertNotNull(testRevision.getId());
-        
-        testRevision.setInfo("Test");
-        revisionFacade.updateRevision(testRevision);
-        Assert.assertEquals("Test", revisionFacade.findById(testRevision.getId()).getInfo());
-    }
+        when(revisionService.create(any(Revision.class))).thenReturn(revision);
+        RevisionDTO rDTO = mappingService.mapTo(revision, RevisionDTO.class);
+        revisionFacade.createRevision(rDTO);
+        Assert.assertNotNull(rDTO.getId());
+    }*/
 }
