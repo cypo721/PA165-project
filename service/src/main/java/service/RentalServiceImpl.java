39eae43d9c218edd538f12cc01da088b130b8502
Venca24 <Vaclav.Zouzalik@seznam.cz>
2017-01-05 22:29:33 +0100

    fix of RentalServiceImpl

--- service/src/main/java/service/RentalServiceImpl.java
+++ service/src/main/java/service/RentalServiceImpl.java
@@ -37,7 +37,7 @@ public class RentalServiceImpl implements RentalService {
     @Override
     public void update(Rental rental) {
         Validate.notNull(rental.getId());
-        rentalDao.create(rental);
+        rentalDao.update(rental);
     }
     
     @Override
