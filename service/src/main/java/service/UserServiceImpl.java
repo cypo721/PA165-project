1de9b9a683d6eeb7a7b5f43dd44e5c85b5a21ef9
Venca24 <Vaclav.Zouzalik@seznam.cz>
2017-01-06 15:21:10 +0100

    extraction of password encryption from UserSeviceImpl in order not to violate the single-responsibility priciple

--- service/src/main/java/service/UserServiceImpl.java
+++ service/src/main/java/service/UserServiceImpl.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import java.math.BigInteger;
-import java.security.SecureRandom;
+import security.PasswordEncryption;
 import java.util.List;
 
 /*
@@ -35,7 +32,7 @@ public class UserServiceImpl implements UserService {
         if (password == null) throw new IllegalArgumentException("Password is null.");
         try
         {
-            usr.setPasswordHash(createHash(password));
+            usr.setPasswordHash(PasswordEncryption.createHash(password));
             userdao.create(usr);
         }
         catch(Exception e)
@@ -51,7 +48,7 @@ public class UserServiceImpl implements UserService {
     {
         if (newPassword == null) throw new IllegalArgumentException("Password is null.");
         User usr = getUserById(id);
-        usr.setPasswordHash(createHash(newPassword));
+        usr.setPasswordHash(PasswordEncryption.createHash(newPassword));
     }
     
     @Override
@@ -59,7 +56,7 @@ public class UserServiceImpl implements UserService {
     {
         if (password == null) throw new IllegalArgumentException("Password is null.");
         User usr = getUserById(id);
-        return validatePassword(password, usr.getPasswordHash());
+        return PasswordEncryption.validatePassword(password, usr.getPasswordHash());
     }
     
     @Override
@@ -109,67 +106,5 @@ public class UserServiceImpl implements UserService {
         {
             throw new TransientDataAccessResourceException("Finding of all users failed", e);
         }
-    }
-    
-    private static String createHash(String password)
-    {
-        final int SALT_BYTE_SIZE = 24;
-        final int HASH_BYTE_SIZE = 24;
-        final int PBKDF2_ITERATIONS = 1000;
-        // Generate a random salt
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[SALT_BYTE_SIZE];
-        random.nextBytes(salt);
-        // Hash the password
-        byte[] hash = pbkdf2(password.toCharArray(), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
-        // format iterations:salt:hash
-        return PBKDF2_ITERATIONS + ":" + toHex(salt) + ":" + toHex(hash);
-    }
-
-    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations, int bytes)
-    {
-        try {
-            PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, bytes * 8);
-            return SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256").generateSecret(spec).getEncoded();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    private static boolean validatePassword(String password, String correctHash)
-    {
-        if(password==null) return false;
-        if(correctHash==null) throw new IllegalArgumentException("password hash is null");
-        String[] params = correctHash.split(":");
-        int iterations = Integer.parseInt(params[0]);
-        byte[] salt = fromHex(params[1]);
-        byte[] hash = fromHex(params[2]);
-        byte[] testHash = pbkdf2(password.toCharArray(), salt, iterations, hash.length);
-        return slowEquals(hash, testHash);
-    }
-    
-    private static boolean slowEquals(byte[] a, byte[] b)
-    {
-        int diff = a.length ^ b.length;
-        for (int i = 0; i < a.length && i < b.length; i++)
-            diff |= a[i] ^ b[i];
-        return diff == 0;
-    }
-    
-    private static byte[] fromHex(String hex)
-    {
-        byte[] binary = new byte[hex.length() / 2];
-        for (int i = 0; i < binary.length; i++) {
-            binary[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
-        }
-        return binary;
-    }
-
-    private static String toHex(byte[] array)
-    {
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
-        return paddingLength > 0 ? String.format("%0" + paddingLength + "d", 0) + hex : hex;
     }
 }
