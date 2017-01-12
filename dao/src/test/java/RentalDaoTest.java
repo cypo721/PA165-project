ef69adea6df63a6a11ca2a3e0ce8c518d02c7fa8
Eduard Cihuňka <eduard@Eduards-MacBook-Pro.local>
2017-01-12 13:49:58 +0100

    fixed dao tests

--- dao/src/test/java/RentalDaoTest.java
+++ dao/src/test/java/RentalDaoTest.java
@@ -1,53 +1,53 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//
-//import config.PersistenceSampleApplicationContext;
-//import dao.MachineDao;
-//import dao.RentalDao;
-//import dao.UserDao;
-//import entity.Machine;
-//import entity.Rental;
-//import entity.User;
-//import enums.MachineType;
-//import enums.PersonType;
-//import enums.Role;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.TestExecutionListeners;
-//import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-//import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.testng.Assert;
-//import org.testng.annotations.*;
-//
-//import java.math.BigDecimal;
-//import java.util.Calendar;
-//import java.util.GregorianCalendar;
-//
-///**
-// *
-// * @author Václav Zouzalík
-// */
-//@Transactional
-//@TestExecutionListeners(TransactionalTestExecutionListener.class)
-//@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
-//public class RentalDaoTest extends AbstractTestNGSpringContextTests {
-//
-//    @Autowired
-//    private RentalDao rentalDao;
-//
-//    @Autowired
-//    private MachineDao machineDao;
-//
-//    @Autowired
-//    private UserDao userDao;
-//
-//    private Machine machine1;
-//    private User usr;
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import config.PersistenceSampleApplicationContext;
+import dao.MachineDao;
+import dao.RentalDao;
+import dao.UserDao;
+import entity.Machine;
+import entity.Rental;
+import entity.User;
+import enums.MachineType;
+import enums.PersonType;
+import enums.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Václav Zouzalík
+ */
+@Transactional
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class RentalDaoTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private RentalDao rentalDao;
+
+    @Autowired
+    private MachineDao machineDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    Calendar cal1 = Calendar.getInstance();
+
+
 //    @BeforeClass
 //    public void beforeClass(){
 //        machine1 = new Machine();
@@ -77,89 +77,139 @@
 //        usr.setRole(Role.EMPLOYEE);
 //        userDao.create(usr);
 //    }
-//
-//    @Test
-//    public void testCreate(){
-//        Rental rent = getRental();
-//        rent.setMachine(machine1);
-//        rent.setUser(usr);
-//
-//        rentalDao.create(rent);
-//
-//        Assert.assertNotNull(rent.getId());
-//        Assert.assertEquals(usr, rent.getUser());
-//        Assert.assertEquals(machine1, rent.getMachine());
-//    }
-//
-//    @Test
-//    public void testfindById(){
-//        Rental rent = getRental();
-//        rent.setMachine(machine1);
-//        rent.setUser(usr);
-//
-//        rentalDao.create(rent);
-//        Assert.assertEquals(rent.getUser().getEmail(), rentalDao.findById(rent.getId()).getUser().getEmail());
-//        Assert.assertEquals(rent.getMachine().getName(), rentalDao.findById(rent.getId()).getMachine().getName());
-//        Assert.assertEquals(new Integer(5000), rentalDao.findById(rent.getId()).getPrice());
-//    }
-//
-//    @Test
-//    public void testUpdate(){
-//        Rental rent = getRental();
-//        rent.setMachine(machine1);
-//        rent.setUser(usr);
-//        rentalDao.create(rent);
-//
-//        rent.setPrice(8500);
-//
-//        rentalDao.update(rent);
-//        Assert.assertEquals(new Integer(8500), rentalDao.findById(rent.getId()).getPrice());
-//    }
-//
-//    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
-//    public void testNullUser(){
-//        Rental rent = getRental();
-//        rent.setUser(null);
-//        rent.setMachine(machine1);
-//
-//        rentalDao.create(rent);
-//    }
-//
-//    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
-//    public void testNullMachine(){
-//        Rental rent = getRental();
-//        rent.setUser(usr);
-//        rent.setMachine(null);
-//
-//        rentalDao.create(rent);
-//    }
-//
-//    @Test
-//    public void testEquals() {
-//        Rental rent1 = getRental();
-//
-//        Assert.assertEquals(rent1, rent1);
-//
-//        Rental rent2 = getRental();
-//        rent2.setPrice(12000);
-//
-//        Assert.assertNotEquals(rent1, rent2);
-//    }
-//
-//    private Rental getRental() {
-//        Rental rent = new Rental();
-//
-//        Calendar cal1 = new GregorianCalendar();
-//        cal1.set(2016, Calendar.OCTOBER, 30, 0, 0, 0);
-//        rent.setDateFrom(cal1.getTime());
-//
-//        Calendar cal2 = Calendar.getInstance();
-//        cal2.set(2016, Calendar.NOVEMBER, 1, 0, 0, 0);
-//        rent.setDateTo(cal2.getTime());
-//
-//        rent.setPrice(5000);
-//
-//        return rent;
-//    }
-//
-//}
+
+    @Test
+    public void testCreate(){
+        Rental rent = getRental();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User usr = getUser();
+        userDao.create(usr);
+        
+        rent.setMachine(machine1);
+        rent.setUser(usr);
+        
+        rentalDao.create(rent);
+
+        Assert.assertNotNull(rent.getId());
+        Assert.assertEquals(usr, rent.getUser());
+        Assert.assertEquals(machine1, rent.getMachine());
+    }
+
+    @Test
+    public void testfindById(){
+        Rental rent = getRental();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User usr = getUser();
+        userDao.create(usr);
+        rent.setMachine(machine1);
+        rent.setUser(usr);
+
+        rentalDao.create(rent);
+        Assert.assertEquals(rent.getUser().getEmail(), rentalDao.findById(rent.getId()).getUser().getEmail());
+        Assert.assertEquals(rent.getMachine().getName(), rentalDao.findById(rent.getId()).getMachine().getName());
+        Assert.assertEquals(new Integer(5000), rentalDao.findById(rent.getId()).getPrice());
+    }
+
+    @Test
+    public void testUpdate(){
+        Rental rent = getRental();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User usr = getUser();
+        userDao.create(usr);
+        rent.setMachine(machine1);
+        rent.setUser(usr);
+        rentalDao.create(rent);
+
+        rent.setPrice(8500);
+        rentalDao.update(rent);
+        Assert.assertEquals(new Integer(8500), rentalDao.findById(rent.getId()).getPrice());
+    }
+
+    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+    public void testNullUser(){
+        Rental rent = getRental();
+        rent.setUser(null);
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        rent.setMachine(machine1);
+
+        rentalDao.create(rent);
+    }
+
+    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+    public void testNullMachine(){
+        Rental rent = getRental();
+        User usr = getUser();
+        userDao.create(usr);
+        rent.setUser(usr);
+        rent.setMachine(null);
+
+        rentalDao.create(rent);
+    }
+
+    @Test
+    public void testEquals() {
+        Rental rent1 = getRental();
+
+        Assert.assertEquals(rent1, rent1);
+
+        Rental rent2 = getRental();
+        rent2.setPrice(12000);
+
+        Assert.assertNotEquals(rent1, rent2);
+    }
+
+    private Rental getRental() {
+        Rental rent = new Rental();
+
+        Calendar cal1 = new GregorianCalendar();
+        cal1.set(2016, Calendar.OCTOBER, 30, 0, 0, 0);
+        rent.setDateFrom(cal1.getTime());
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(2016, Calendar.NOVEMBER, 1, 0, 0, 0);
+        rent.setDateTo(cal2.getTime());
+
+        rent.setPrice(5000);
+
+        return rent;
+    }
+
+    private Machine getMachine() {
+        Machine machine1 = new Machine();
+        machine1.setName("Cat");
+        machine1.setPricePerDay(BigDecimal.TEN);
+        machine1.setMachineType(MachineType.EXCAVATOR);
+        cal1.set(Calendar.YEAR, 2014);
+        cal1.set(Calendar.MONTH, Calendar.JANUARY);
+        cal1.set(Calendar.DAY_OF_MONTH, 6);
+        machine1.setDateOfBuy(cal1.getTime());
+        machine1.setDateOfLastRevision(cal1.getTime());
+        
+        return machine1;
+    }
+    
+    private User getUser() {
+        User usr = new User();
+        usr.setGivenName("Albus");
+        usr.setSurname("Dumbledore");
+        usr.setPasswordHash("adfbgnh");
+        usr.setEmail("albus@hogwarts.edu");
+        usr.setPersonType(PersonType.NATURAL);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(Calendar.YEAR, 2016);
+        cal2.set(Calendar.MONTH, Calendar.JANUARY);
+        cal2.set(Calendar.DAY_OF_MONTH, 20);
+        usr.setJoinedDate(cal1.getTime());
+        usr.setPhone("800123456");
+        usr.setRole(Role.EMPLOYEE);
+        
+        return usr;
+    }
+}
--- dao/src/test/java/RevisionDaoTest.java
+++ dao/src/test/java/RevisionDaoTest.java
@@ -1,43 +1,43 @@
-//import config.PersistenceSampleApplicationContext;
-//import dao.MachineDao;
-//import dao.RevisionDao;
-//import dao.UserDao;
-//import entity.Machine;
-//import entity.Revision;
-//import entity.User;
-//import enums.MachineType;
-//import enums.PersonType;
-//import enums.Role;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.TestExecutionListeners;
-//import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-//import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.testng.Assert;
-//import org.testng.annotations.BeforeClass;
-//import org.testng.annotations.Test;
-//
-//import java.math.BigDecimal;
-//import java.util.Calendar;
-//
-///**
-// * Created by pato on 31.10.2016.
-// */
-//@Transactional
-//@TestExecutionListeners(TransactionalTestExecutionListener.class)
-//@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
-//public class RevisionDaoTest extends AbstractTestNGSpringContextTests {
-//
-//    @Autowired
-//    private RevisionDao revisionDao;
-//    @Autowired
-//    private MachineDao machineDao;
-//    @Autowired
-//    private UserDao userDao;
-//    private User u;
-//    private Machine machine1;
-//
+import config.PersistenceSampleApplicationContext;
+import dao.MachineDao;
+import dao.RevisionDao;
+import dao.UserDao;
+import entity.Machine;
+import entity.Revision;
+import entity.User;
+import enums.MachineType;
+import enums.PersonType;
+import enums.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+/**
+ * Created by pato on 31.10.2016.
+ */
+@Transactional
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
+public class RevisionDaoTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private RevisionDao revisionDao;
+    @Autowired
+    private MachineDao machineDao;
+    @Autowired
+    private UserDao userDao;
+
+    Calendar cal1 = Calendar.getInstance();
+    
 //    @BeforeClass
 //    public void beforeClass(){
 //        machine1 = new Machine();
@@ -67,94 +67,154 @@
 //        u.setRole(Role.EMPLOYEE);
 //        userDao.create(u);
 //    }
-//
-//    @Test
-//    public void testCreate(){
-//        Revision r = getRevision();
-//        r.setMachine(machine1);
-//        r.setUser(u);
-//
-//        revisionDao.create(r);
-//
-//        Assert.assertNotNull(r.getId());
-//        Assert.assertEquals(u, r.getUser());
-//        Assert.assertEquals(machine1, r.getMachine());
-//    }
-//
-//    @Test
-//    public void testfindById(){
-//        Revision r = getRevision();
-//        r.setMachine(machine1);
-//        r.setUser(u);
-//
-//        revisionDao.create(r);
-//        Assert.assertEquals(r.getUser().getEmail(), revisionDao.findById(r.getId()).getUser().getEmail());
-//        Assert.assertEquals(r.getMachine().getName(), revisionDao.findById(r.getId()).getMachine().getName());
-//    }
-//
-//    @Test
-//    public void testUpdate(){
-//        Revision r = getRevision();
-//        r.setMachine(machine1);
-//        r.setUser(u);
-//        revisionDao.create(r);
-//
-//        r.setInfo("Machine doesn't start.");
-//        r.setIsFunctionable(false);
-//        revisionDao.update(r);
-//        Assert.assertEquals("Machine doesn't start.", revisionDao.findById(r.getId()).getInfo());
-//        Assert.assertEquals(false, revisionDao.findById(r.getId()).getIsFunctionable());
-//    }
-//
-//
-//
-//    @Test
-//    public void createFindDeleteTest(){
-//        Revision r = getRevision();
-//        r.setMachine(machine1);
-//        r.setUser(u);
-//
-//        revisionDao.create(r);
-//        Revision r2 = getRevision();
-//        r2.setMachine(machine1);
-//        r2.setUser(u);
-//
-//        revisionDao.create(r2);
-//        Assert.assertEquals(revisionDao.findById(r.getId()).getUser(), u);
-//        Assert.assertEquals(revisionDao.findAllRevisions().size(), 2);
-//        revisionDao.delete(r2);
-//        Assert.assertEquals(revisionDao.findAllRevisions().size(), 1);
-//        revisionDao.delete(r);
-//        Assert.assertEquals(revisionDao.findAllRevisions().size(), 0);
-//    }
-//
-//    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
-//    public void testNullUser(){
-//        Revision r = getRevision();
-//        r.setMachine(machine1);
-//
-//        revisionDao.create(r);
-//    }
-//
-//    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
-//    public void testNullMachine(){
-//        Revision r = getRevision();
-//        r.setUser(u);
-//
-//        revisionDao.create(r);
-//    }
-//
-//    private Revision getRevision() {
-//        Revision r = new Revision();
-//        Calendar cal1 = Calendar.getInstance();
-//        cal1.set(Calendar.YEAR, 2012);
-//        cal1.set(Calendar.MONTH, 0);
-//        cal1.set(Calendar.DAY_OF_MONTH, 20);
-//        r.setDateOfRevision(cal1.getTime());
-//        r.setInfo("Machine was revisioned. No problems found.");
-//        r.setIsFunctionable(true);
-//
-//        return r;
-//    }
-//
-//}
+
+    @Test
+    public void testCreate(){
+        Revision r = getRevision();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User u = getUser();
+        userDao.create(u);
+        r.setMachine(machine1);
+        r.setUser(u);
+
+        revisionDao.create(r);
+
+        Assert.assertNotNull(r.getId());
+        Assert.assertEquals(u, r.getUser());
+        Assert.assertEquals(machine1, r.getMachine());
+    }
+
+    @Test
+    public void testfindById(){
+        Revision r = getRevision();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User u = getUser();
+        userDao.create(u);
+        r.setMachine(machine1);
+        r.setUser(u);
+
+        revisionDao.create(r);
+        Assert.assertEquals(r.getUser().getEmail(), revisionDao.findById(r.getId()).getUser().getEmail());
+        Assert.assertEquals(r.getMachine().getName(), revisionDao.findById(r.getId()).getMachine().getName());
+    }
+
+    @Test
+    public void testUpdate(){
+        Revision r = getRevision();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User u = getUser();
+        userDao.create(u);
+        r.setMachine(machine1);
+        r.setUser(u);
+        revisionDao.create(r);
+
+        r.setInfo("Machine doesn't start.");
+        r.setIsFunctionable(false);
+        revisionDao.update(r);
+        Assert.assertEquals("Machine doesn't start.", revisionDao.findById(r.getId()).getInfo());
+        Assert.assertEquals(false, revisionDao.findById(r.getId()).getIsFunctionable());
+    }
+
+
+
+    @Test
+    public void createFindDeleteTest(){
+        Revision r = getRevision();
+        
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        User u = getUser();
+        userDao.create(u);
+        
+        r.setMachine(machine1);
+        r.setUser(u);
+
+        revisionDao.create(r);
+        Revision r2 = getRevision();
+        r2.setMachine(machine1);
+        r2.setUser(u);
+
+        revisionDao.create(r2);
+        Assert.assertEquals(revisionDao.findById(r.getId()).getUser(), u);
+        Assert.assertEquals(revisionDao.findAllRevisions().size(), 2);
+        revisionDao.delete(r2);
+        Assert.assertEquals(revisionDao.findAllRevisions().size(), 1);
+        revisionDao.delete(r);
+        Assert.assertEquals(revisionDao.findAllRevisions().size(), 0);
+    }
+
+    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+    public void testNullUser(){
+        Revision r = getRevision();
+        Machine machine1 = getMachine();
+        machineDao.create(machine1);
+        
+        r.setMachine(machine1);
+
+        revisionDao.create(r);
+    }
+
+    @Test(expectedExceptions = javax.validation.ConstraintViolationException.class)
+    public void testNullMachine(){
+        Revision r = getRevision();
+        
+        User u = getUser();
+        userDao.create(u);
+        r.setUser(u);
+
+        revisionDao.create(r);
+    }
+
+    private Revision getRevision() {
+        Revision r = new Revision();
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(Calendar.YEAR, 2012);
+        cal1.set(Calendar.MONTH, 0);
+        cal1.set(Calendar.DAY_OF_MONTH, 20);
+        r.setDateOfRevision(cal1.getTime());
+        r.setInfo("Machine was revisioned. No problems found.");
+        r.setIsFunctionable(true);
+
+        return r;
+    }
+    
+    private Machine getMachine() {
+        Machine machine1 = new Machine();
+        machine1.setName("Cat");
+        machine1.setPricePerDay(BigDecimal.TEN);
+        machine1.setMachineType(MachineType.EXCAVATOR);
+        cal1.set(Calendar.YEAR, 2014);
+        cal1.set(Calendar.MONTH, Calendar.JANUARY);
+        cal1.set(Calendar.DAY_OF_MONTH, 6);
+        machine1.setDateOfBuy(cal1.getTime());
+        machine1.setDateOfLastRevision(cal1.getTime());
+        
+        return machine1;
+    }
+    
+    private User getUser() {
+        User usr = new User();
+        usr.setGivenName("Albus");
+        usr.setSurname("Dumbledore");
+        usr.setPasswordHash("adfbgnh");
+        usr.setEmail("albus@hogwarts.edu");
+        usr.setPersonType(PersonType.NATURAL);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(Calendar.YEAR, 2016);
+        cal2.set(Calendar.MONTH, Calendar.JANUARY);
+        cal2.set(Calendar.DAY_OF_MONTH, 20);
+        usr.setJoinedDate(cal1.getTime());
+        usr.setPhone("800123456");
+        usr.setRole(Role.EMPLOYEE);
+        
+        return usr;
+    }
+
+}
